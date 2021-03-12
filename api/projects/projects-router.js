f2c088c63465dfe17723d51c15f933a2b3e07f81
Nick <nwestallen@gmail.com>
2021-03-12 13:55:26 -0500

    DELETE /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -16,7 +16,19 @@ router.get('/:id', validateProjectId, (req, res) => {
 
 router.post('/', validateNewProject, (req, res, next) => {
   Project.insert(req.project)
+    .then(res.status(201).json(req.project))
+    .catch(next);
+});
+
+router.put('/:id', validateProjectId, validateNewProject, (req, res, next) => {
+  Project.update(req.params.id, req.project)
     .then(res.json(req.project))
+    .catch(next);
+});
+
+router.delete('/:id', validateProjectId, (req, res, next) => {
+  Project.remove(req.params.id)
+    .then(res.json({message: `project with id ${req.params.id} successfully removed`}))
     .catch(next);
 });
 
